b61321de5a6ac229b42e2e9bf7a054e2829b33a9
Marchael Acode <m.acode@outlook.com>
2021-03-19 23:51:51 -0700

    Timer counts down from 10 before starting workout

--- app/src/main/java/com/macode/a10minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/macode/a10minuteworkout/ExerciseActivity.kt
@@ -2,6 +2,8 @@ package com.macode.a10minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.macode.a10minuteworkout.databinding.ActivityExerciseBinding
 import com.macode.a10minuteworkout.databinding.MainToolbarBinding
@@ -9,6 +11,8 @@ import com.macode.a10minuteworkout.databinding.MainToolbarBinding
 class ExerciseActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityExerciseBinding
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +26,39 @@ class ExerciseActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setUpRestView()
+    }
+
+    override fun onDestroy() {
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+    }
+
+    private fun setRestProgressBar() {
+        binding.progressBar.progress = restProgress
+        restTimer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding.progressBar.progress = 10 - restProgress
+                binding.timer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Let's go!", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    private fun setUpRestView() {
+        if (restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
     }
 }
